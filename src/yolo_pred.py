e6f6a04a9ac15d739cec38d731c0410fa75cfba5
chamani-parisa <148037710+chamani-parisa@users.noreply.github.com>
2026-06-10 23:14:15 +0330

    Update yolo_pred.py

--- src/yolo_pred.py
+++ src/yolo_pred.py
@@ -20,4 +20,65 @@ def pred_yolo(img,model):
     return pred_mask
 
 
+def creat_n_colors(n=32):
+    m = n + 1
+
+    color_list = np.zeros((m, 1, 3), np.uint8)
+
+    color_list[:, :, 1] = 255
+    color_list[:, :, 2] = 255
+
+    color_list[:, :, 0] = np.linspace(0, 180, m).reshape((-1, 1)).astype(np.uint8)
+    color_list = cv2.cvtColor(color_list, cv2.COLOR_HSV2BGR)
+    color_list = color_list.reshape((-1, 3))
+
+    color_list = [(int(c[0]), int(c[1]), int(c[2])) for c in color_list]
+
+    return color_list[:-1]
+
+
+yolo_color_list = creat_n_colors(n=32)
+def create_pred_img(img,model):
+
+    res = model.predict(img,device=0,verbose=False)[0]
+
+    overwrite = img.copy()
+
+    if res.masks is None:
+        return img
+
+    for box,mask in zip(res.boxes,res.masks):
+        xy = mask.xy[0].astype(np.int32)
+        cls = int(box.cls[0].item())
+        conf = round(box.conf[0].cpu().item(),2)
+
+        x1,y1,x2,y2 = box.xyxy[0].cpu().numpy().astype(int).tolist()
+
+
+        cv2.fillPoly(overwrite, [xy], yolo_color_list[cls])
+        cv2.drawContours(img, [xy], 0,  yolo_color_list[cls], 2)
+
+        cv2.putText(img,f'{cls},{conf}',(x1,y1-5),cv2.FONT_HERSHEY_SIMPLEX,1,yolo_color_list[cls],2)
+        cv2.putText(overwrite, f'{cls},{conf}', (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 1, yolo_color_list[cls], 2)
+
+
+    img = cv2.addWeighted(img,0.5,overwrite,0.5,0,img)
+
+    return img
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
